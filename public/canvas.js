e2fbd0343b60d70555253441fcf45c3b695bc291
Mehmet Kahya <mehmetkahyakas5@gmail.com>
2024-07-09 14:02:39 +0300

    chore: Remove deprecated canvas.js and username.js files, update canvas.html and style.css for online user functionality

--- public/canvas.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    const params = new URLSearchParams(window.location.search);
-    const username = params.get('username');
-    document.getElementById('usernameDisplay').textContent = username;
-};
